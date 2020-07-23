f0360407e8a86558270d2331dc2de92b4f4d6499
cduboff <clbduboff@gmail.com>
2020-07-23 07:57:26 -0500

    more changes

--- dojo_survey_revisited/app_one/views.py
+++ dojo_survey_revisited/app_one/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render, HttpResponse, redirect
 
+LOC = (
+    'California',
+    'Online',
+    'Chicago'
+)
+
+LANG = (
+    'Python',
+    'Java',
+    'JavaScript',
+    'Ruby',
+    'None'
+)
+
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'location': LOC,
+        'favorite_language': LANG
+    }
+    return render(request, 'index.html', context)
 def show(request):
     if request.method == 'GET':
         return redirect('/')
@@ -9,7 +27,7 @@ def show(request):
         'name': request.POST['name'],
         'loc': request.POST['location'],
         'lang': request.POST['favorite_language'],
-        'comment': request.POST['comment'],
+        'comment': request.POST['comment']
     }
     return redirect('/result')
 def result(request):
