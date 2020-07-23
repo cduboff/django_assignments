8179981ced2ce1b9cd886a42df212c2fe41ebc19
cduboff <clbduboff@gmail.com>
2020-07-23 07:27:48 -0500

    edits to dojo_survey

--- dojo_survey_revisited/app_one/views.py
+++ dojo_survey_revisited/app_one/views.py
@@ -2,22 +2,19 @@ from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
     return render(request, 'index.html')
-def result(request):
-    request.session['name'] = request.POST['name']
-    request.session['loc'] = request.POST['location']
-    request.session['lang'] = request.POST['favorite_language']
-    request.session['comment'] = request.POST['comment']
+def show(request):
+    if request.method == 'GET':
+        return redirect('/')
+    request.session['result'] = {
+        'name': request.POST['name'],
+        'loc': request.POST['location'],
+        'lang': request.POST['favorite_language'],
+        'comment': request.POST['comment'],
+    }
     return redirect('/result')
-# def result(request):
-#     if request.method == "GET":
-#         print(request.GET)
-#     if request.method == "POST":
-#         context = {
-#             'name': request.POST['name'],
-#             'lang': request.POST['favorite_language'],
-#             'loc': request.POST['location'],
-#             'comment': request.POST['comment'],
-#         }
-#         return render(request, 'result.html', context)
-#     return redirect('/result')
+def result(request):
+    context = {
+        'result': request.session['result']
+    }
+    return render(request, 'result.html', context)
 # Create your views here.
